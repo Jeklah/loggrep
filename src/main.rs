7b96703c19e190ed54e04f53485f0a5bb14774bc
Arthur Bowers <arthur.bowers@phabrix.com>
2025-11-03 11:58:11 +0000

    Adding use of Clap to handle argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,44 @@
-use std::env;
+use clap::Parser;
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::BufRead;
 use std::path::Path;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        eprintln!("Usage: loggrep <logfile> <search_term>");
-        std::process::exit(1);
-    }
+#[derive(Parser)]
+#[command(name = "loggrep")]
+#[command(about = "A lightweight log filtering tool made in Rust")]
+struct Args {
+    #[arg(help = "Path to log file")]
+    file: String,
 
-    let path = &args[1];
-    let search_term = &args[2];
-    if let Ok(lines) = read_lines(path) {
-        for line in lines.flatten() {
-            if line.contains(search_term) {
-                println!("{}", line);
+    #[arg(help = "Keyword to search for")]
+    keyword: String,
+
+    #[arg(short, long, help = "Show line numbers")]
+    lines: bool,
+}
+
+// Main function
+fn main() {
+    let args = Args::parse();
+
+    if let Ok(lines) = read_lines(&args.file) {
+        for (index, line) in lines.flatten().enumerate() {
+            if line.contains(&args.keyword) {
+                if args.lines {
+                    println!("{}: {}", index + 1, line);
+                } else {
+                    println!("{}", line);
+                }
             }
         }
     }
 }
 
 // Function to read lines from a file and return an iterator
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+fn read_lines<P>(filename: P) -> std::io::Result<std::io::Lines<std::io::BufReader<File>>>
 where
     P: AsRef<Path>,
 {
     let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    Ok(std::io::BufReader::new(file).lines())
 }
-
